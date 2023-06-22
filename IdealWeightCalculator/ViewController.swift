0fe2d254de4e7f0caeda86d24883e609f534fa99
Martha Moreno <88118070+martha-moreno@users.noreply.github.com>
2023-06-22 09:01:49 -0700

    Initial Commit

---
+++ IdealWeightCalculator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  IdealWeightCalculator
+//
+//  Created by Martha Moreno on 6/22/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
